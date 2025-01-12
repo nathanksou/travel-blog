e539c569c7fcca4bbe2ad002a7d9d45baafe48de
Nathan Sou <nathanksou@gmail.com>
2025-01-12 21:40:13 +1100

    init

---
+++ tailwindcss.d.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+declare module "tailwindcss/lib/util/flattenColorPalette" {
+  function flattenColorPalette(palette: any): any;
+  export = flattenColorPalette;
+}
