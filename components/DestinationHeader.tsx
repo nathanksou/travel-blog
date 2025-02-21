0a29f90d2d945759c292d36f470a7e813b69752c
Nathan Sou <nathanksou@gmail.com>
2025-02-21 12:51:00 +1100

    update type on destination header

--- components/DestinationHeader.tsx
+++ components/DestinationHeader.tsx
@@ -1,12 +1,8 @@
 import { ChevronLeftIcon } from "@heroicons/react/24/solid";
-import { DestinationMeta } from "@/lib/types";
+import { Destination } from "@/lib/types";
 import Link from "next/link";
 
-export function DestinationHeader({
-  title,
-}: {
-  title: DestinationMeta["name"];
-}) {
+export function DestinationHeader({ title }: { title: Destination["name"] }) {
   return (
     <header className="flex flex-start items-center">
       <Link href="/" aria-label="Back to home">
